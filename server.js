d807447b1282a012666b4c72a193381c27b2da22
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-01 12:04:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express')
 const fs = require('fs')
-co
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+
+// caminho para o arquivo artists.json
+const artistsFilePath = path.join(__dirname,'api-artists','artists.json')
+// todos os artistas
+app.get('/artists',(req,res) =>{
+    //ler o arquivo artists.json
+fs.readFile(artistsFilePath,(err,data) =>{
+    if(err){
+        return res.status(500).json({error:"Erro ao ler os dados dos artistas"})
+    }
+    const artist
+})
+})
