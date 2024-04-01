049c792152f09884b661e71a76b831b9336953e4
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-01 12:21:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+// const express = require('express')
+// const fs = require('fs')
+// const path = require('path')
 
-const app = express()
-const PORT = 3000
+// const app = express()
+// const PORT = 3000
 
-// caminho para o arquivo artists.json
-const artistsFilePath = path.join(__dirname,'api-artists','artists.json')
-// todos os artistas
-app.get('/artists',(req,res) =>{
-    //ler o arquivo artists.json
-fs.readFile(artistsFilePath,(err,data) =>{
-    if(err){
-        return res.status(500).json({error:"Erro ao ler os dados dos artistas"})
-    }
-    const artist = JSON.parse(data)
-    res.json(artist)
-    })
-})
-// inicia o servidor 
-app.listen(PORT,() => {
-    console.log('Servidor rodando em http://localhost:$3000]')
-})
+// // caminho para o arquivo artists.json
+// const artistsFilePath = path.join(__dirname,'api-artists','artists.json')
+// // todos os artistas
+// app.get('/artists',(req,res) =>{
+//     //ler o arquivo artists.json
+// fs.readFile(artistsFilePath,(err,data) =>{
+//     if(err){
+//         return res.status(500).json({error:"Erro ao ler os dados dos artistas"})
+//     }
+//     const artist = JSON.parse(data)
+//     res.json(artist)
+//     })
+// })
+// // inicia o servidor 
+// app.listen(PORT,() => {
+//     console.log('Servidor rodando em http://localhost:$3000]')
+// })
